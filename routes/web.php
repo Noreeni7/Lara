34d17494d75d72dcc7a8b14610bd0d5f609c804e
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-11-09 16:02:17 +0300

    controller resource methods

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 
 // =========================== GET route example ==================================
